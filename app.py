91b977cb73a5ada1ac57886dd3310303a9b2a4e5
BAO Chengheri <chengheri.bao@hotmail.com>
2024-12-28 16:38:29 +0000

    fix: response not defined outside the form

--- app.py
+++ app.py
@@ -11,26 +11,26 @@ with st.sidebar:
 st.title("📑 Write a blog post")
 st.caption(" A streamlit blog post writer powered by OpenAI and AutoGen")
 
-with st.form("topic"):
-    topic = st.text_area("What is the topic of your blog post ?", "")
-    num_words = st.number_input(
-        "The number of words of your blog post :", value=100, placeholder="Type a number..."
-    )
-    submitted = st.form_submit_button("Submit")
-    if not openai_api_key:
-        st.info("Please add your OpenAI API key to continue.")
-    elif submitted:
-        task = Task(topic, num_words).get_task()
-        llm_config = {"model": "gpt-3.5-turbo", "api_key": openai_api_key}
-        orchestrator = Orchestrator(llm_config)
-        response = orchestrator.generate_response(task)
-        writer_responses = orchestrator.get_writer_responses(response)
-        initial_version = writer_responses[0]
-        refined_version = writer_responses[1]
-        st.write("### Initial version:")
-        st.write(initial_version)
-        st.write("### Refined version:")
-        st.write(refined_version)
+form = st.form("blog post")
+topic = st.text_area("What is the topic of your blog post ?", "")
+num_words = st.number_input(
+    "The number of words of your blog post :", value=100, placeholder="Type a number..."
+)
+submitted = form.form_submit_button("Submit")
+if not openai_api_key:
+    st.info("Please add your OpenAI API key to continue.")
+elif submitted:
+    task = Task(topic, num_words).get_task()
+    llm_config = {"model": "gpt-3.5-turbo", "api_key": openai_api_key}
+    orchestrator = Orchestrator(llm_config)
+    response = orchestrator.generate_response(task)
+    writer_responses = orchestrator.get_writer_responses(response)
+    initial_version = writer_responses[0]
+    refined_version = writer_responses[1]
+    st.write("### Initial version:")
+    st.write(initial_version)
+    st.write("### Refined version:")
+    st.write(refined_version)
 
 if st.button("Show feedback") and response:
     meta_feedback = orchestrator.get_meta_feedback(response)
