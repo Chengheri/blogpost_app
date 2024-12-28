ad765137f4c98249868cca25f0bd3c370fd1ee4b
BAO Chengheri <chengheri.bao@hotmail.com>
2024-12-28 15:00:44 +0000

    fix: missing arg in instantiation of agent

--- orchestrator.py
+++ orchestrator.py
@@ -3,12 +3,12 @@ from agent import Agent
 class Orchestrator:
 
     def __init__(self):
-        self.writer = Agent.get_writer()
-        self.critic = Agent.get_critic()
-        self.SEO_reviewer = Agent.get_SEO_reviewer()
-        self.legal_reviewer = Agent.get_legal_reviewer()
-        self.ethics_reviewer = Agent.get_ethics_reviewer()
-        self.meta_reviewer = Agent.get_meta_reviewer()
+        self.writer = Agent().get_writer()
+        self.critic = Agent().get_critic()
+        self.SEO_reviewer = Agent().get_SEO_reviewer()
+        self.legal_reviewer = Agent().get_legal_reviewer()
+        self.ethics_reviewer = Agent().get_ethics_reviewer()
+        self.meta_reviewer = Agent().get_meta_reviewer()
 
     def get_nested_chats(self, max_turns:int=1):
         def reflection_message(recipient, sender):
