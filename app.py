46b5c20ccf5f3564b928ca069684019c787dffd8
BAO Chengheri <chengheri.bao@hotmail.com>
2024-12-28 23:36:37 +0000

    add session state

--- app.py
+++ app.py
@@ -21,17 +21,19 @@ st.caption(" A streamlit blog post writer powered by OpenAI and AutoGen")
 
 if "messages" not in st.session_state:
     st.session_state["messages"] = [
-        {"role": "assistant", "content": "Hi, I'm an AI agent who can write blog posts."}
+        {"role": "assistant", "content": "Hi, we are a team of multi agents who can write blog posts."}
     ]
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
-with st.form("blog post"):
-    topic = st.text_area("What is the topic of your blog post ?", "")
-    st.session_state.messages.append({"role": "user", "content": topic})
+if "response" not in st.session_state:
+    st.session_state["response"] = None
+
+with st.form(key="blog_post"):
+    topic = st.text_area("What is the topic of your blog post ?", key="topic")
     num_words = st.number_input(
-        "Which is the expected number of words of your blog post ?", value=100, placeholder="Type a number..."
+        "Which is the expected number of words of your blog post ?", key="num_words", value=100, placeholder="Type a number..."
     )
     submitted = st.form_submit_button("Submit")
     if not openai_api_key:
@@ -41,8 +43,8 @@ with st.form("blog post"):
         llm_config = {"model": model, "api_key": openai_api_key}
         orchestrator = Orchestrator(llm_config)
         response = orchestrator.generate_response(task)
-        st.session_state.messages.append({"role": "assistant", "content": response})
-        writer_responses = orchestrator.get_writer_responses(response)
+        st.session_state["response"] = response
+        writer_responses = orchestrator.get_writer_responses(st.session_state["response"])
         initial_version = writer_responses[0]
         refined_version = writer_responses[1]
         st.write("### Initial version:")
@@ -51,16 +53,16 @@ with st.form("blog post"):
         st.write(refined_version)
         
 
-        meta_feedback = orchestrator.get_meta_feedback(response)
+        meta_feedback = orchestrator.get_meta_feedback(st.session_state["response"])
         stoggle(
             "Show Feedback🖍️",
             meta_feedback,
         )
 
-        show_cost = st.checkbox("Show Cost")
+        show_cost = st.checkbox("Show Cost", key="show_cost")
 
         if show_cost:
-            cost = orchestrator.get_cost(response)
+            cost = orchestrator.get_cost(st.session_state["response"])
             st.write(cost)
        
 
