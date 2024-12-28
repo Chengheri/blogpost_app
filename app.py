cd85dd7921d04a506e22ee9893e4e8a2c28d01c7
BAO Chengheri <chengheri.bao@hotmail.com>
2024-12-28 17:16:05 +0000

    add feedback form

--- app.py
+++ app.py
@@ -32,13 +32,11 @@ with st.form("blog post"):
         st.write("### Refined version:")
         st.write(refined_version)
         
-        meta_feedback = orchestrator.get_meta_feedback(response)
-        st.write("### Feedback")
-        st.write(meta_feedback)
-
-        show_feedback = st.form_submit_button("Show feedback")
-        if show_feedback and response:
-            meta_feedback = orchestrator.get_meta_feedback(response)
-            st.write(meta_feedback)
+        with st.form("feedback"):
+            show_feedback = st.form_submit_button("Show feedback")
+            if show_feedback and response:
+                meta_feedback = orchestrator.get_meta_feedback(response)
+                st.write("### Feedback")
+                st.write(meta_feedback)
        
 
